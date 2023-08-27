d8b78c379c68248bd8d69ac68e2938732a6f809b
charoncode <74079903+charoncode@users.noreply.github.com>
2023-08-27 01:11:49 -0500

    Add files via upload

---
+++ homeworks/hw1_02.m
@@ -0,0 +1,16 @@
+clear, close all; clc;
+
+A= % ADD YOUR CODE HERE
+b= % ADD YOUR CODE HERE
+
+x = % ADD YOUR CODE HERE 
+fprintf('Solution of linear system:\n');
+disp(x)
+
+M = % ADD YOUR CODE HERE
+fprintf('Augmented matrix: \n');
+disp(M)
+
+B=% ADD YOUR CODE HERE
+fprintf('Reduced echelon form: \n');
+disp(B)
