1690ce379d3286477184b697ffeacfbc3b29dd3a
charoncode <74079903+charoncode@users.noreply.github.com>
2023-09-04 17:04:44 -0500

    Create hw04.m

---
+++ homeworks/hw04.m
@@ -0,0 +1,21 @@
+clear, close all; clc;
+
+% Create B
+B= % ADD YOUR CODE HERE
+
+% Define and display C
+C= tril(B);
+
+fprintf('Matrix C: \n');
+disp(C)
+
+% Build (9x1) vector of ones
+x = % ADD YOUR CODE HERE 
+
+% Compute product of C by x
+y= % ADD YOUR CODE HERE 
+
+fprintf('y=\n');
+disp(y)
+
+% Add any comment here
