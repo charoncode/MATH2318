c12933f806a6c63bf9de805af03e1bcd6285129c
charoncode <74079903+charoncode@users.noreply.github.com>
2023-09-12 16:59:12 -0500

    Add files via upload

---
+++ homeworks/hw05.m
@@ -0,0 +1,34 @@
+clear, close all;
+clc;
+
+% define the matrix
+A = [10 -7  1   4  6; ...
+     -8  4 -6 -10 -3; ...
+     -7 11 -5  -1 -8; ...
+      3 -1 10  12 12];
+
+% Question a)
+% Compute and display REF of A
+fprintf('reduced echelon form of A\n');
+% ADD YOUR CODE HERE
+
+% Add your comments here
+
+% Question b)
+% let's start by deleting the first column
+B=% ADD YOUR CODE HERE
+
+% Compute and dsiplay the REF of the new matrix
+fprintf('reduced echelon form of A(:,2:5)\n');
+% ADD YOUR CODE HERE
+
+% Add your comments here
+
+% Question c)
+% Repeat the process by deleting one of each possible column from A and
+% checking the resulting REF in each case. For more advanced students, you
+% may use a for loop for doing that. 
+
+% ADD YOUR CODE HERE
+
+% Add your comments here
