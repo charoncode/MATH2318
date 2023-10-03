0ff3583a8583f2e305f3b913046fcd6aa305cf39
charoncode <74079903+charoncode@users.noreply.github.com>
2023-10-03 00:53:26 -0500

    Add files via upload

---
+++ homeworks/hw08.m
@@ -0,0 +1,17 @@
+A = [31, -8, 11; -8, 15, -6; 11, -6, 25];
+b = [ 8; -4; 13];
+
+% Compute inverse of A
+invA = % ADD YOUR CODE HERE
+
+% Obtain solution of linear system from the inverse
+x = % ADD YOUR CODE HERE
+
+% compute residual vector Ax-b
+r= % ADD YOUR CODE HERE
+
+% display r and the norm of r
+% ADD YOUR CODE HERE
+
+
+
