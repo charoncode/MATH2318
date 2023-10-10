5434b4a338789b871c8e176577cddee2a0851466
charoncode <74079903+charoncode@users.noreply.github.com>
2023-10-10 00:42:41 -0500

    Add files via upload

---
+++ homeworks/hw06_1.m
@@ -0,0 +1,18 @@
+clear, close all; clc; 
+
+for n = 4:6
+    % Build tridiagonal matrix
+    A1 = % ADD YOUR CODE HERE
+    A2 = % ADD YOUR CODE HERE
+    A3 = % ADD YOUR CODE HERE
+
+    A =  % ADD YOUR CODE HERE
+    
+    % compute determinant of A
+    % using the det command
+    detA = % ADD YOUR CODE HERE
+    
+    fprintf('determinant of matrix A:\n'); disp(detA);
+end
+
+% ADD A COMMENT WITH YOUR OBSERVATIONS BELOW
