3b948dc7eb1ed14b5f6e49fe0cd6dab7d00b30e8
charoncode <74079903+charoncode@users.noreply.github.com>
2023-10-10 00:43:52 -0500

    Update hw06_2.m

--- homeworks/hw06_2.m
+++ homeworks/hw06_2.m
@@ -1,12 +1,14 @@
 clear, close all; clc;
+size = 5:3:8; % values of n 
 
-for i = 1 : 3
-    % create a matrix of size 5 x 5 with random entries
+for i = 1 : numel(size)
+    n=size(i);
+    % create a matrices of size n x n with random entries
     % using the rand command
     A = % ADD YOUR CODE HERE
     B = % ADD YOUR CODE HERE
     fprintf('---------------------------------------\n');
-    fprintf('for A,B %d x %d matrices\n', n(i), n(i));
+    fprintf('for A,B %d x %d matrices\n', n, n);
     fprintf('---------------------------------------\n');
     fprintf('matrix A:\n'); disp(A);
     fprintf('matrix B:\n'); disp(B);
